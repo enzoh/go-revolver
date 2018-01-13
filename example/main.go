d913351deb0d31ead97038d23aab97380f8678ad
Enzo Haussecker <enzo@sovereign.io>
2018-01-12 18:04:12 -0800

    Launch multiple clients in a single process

--- example/main.go
+++ example/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crypto/rand"
-	"crypto/sha256"
 	"encoding/hex"
 	"flag"
+	"math"
+	"math/big"
+	"sync"
 	"time"
 
 	"github.com/dfinity/go-revolver/artifact"
@@ -14,12 +16,18 @@ import (
 
 func main() {
 
-	argLevel := flag.String("log-level", "info", "Log level.")
+	// Parse command-line arguments.
+	argClients := flag.Int("clients", 1, "Number of clients.")
+	argDisableNATPortMap := flag.Bool("disable-nat", false, "Disable port-mapping in NAT devices?")
+	argIP := flag.String("ip", "0.0.0.0", "IP address to listen on.")
+	argKBucketSize := flag.Int("k-bucket-size", 8, "Kademlia bucket size.")
+	argLevel := flag.String("log-level", "debug", "Log level.")
 	argPort := flag.Uint("port", 0, "Port number to listen on.")
 	argRandomSeed := flag.String("random-seed", "", "32-byte hex-encoded random seed.")
 	argSeedNode := flag.String("seed-node", "", "Address of seed node.")
 	flag.Parse()
 
+	// Create a logger.
 	var level logging.Level
 	logger := logging.MustGetLogger("main")
 	level, err := logging.LogLevel(*argLevel)
@@ -28,63 +36,116 @@ func main() {
 		return
 	}
 	logging.SetLevel(level, "main")
+	logging.SetLevel(level, "p2p")
 
-	config := p2p.DefaultConfig()
-	config.DisableAnalytics = true
-	config.LogLevel = *argLevel
-	config.Port = (uint16(*argPort))
-	config.RandomSeed = *argRandomSeed
-	if *argSeedNode != "" {
-		config.SeedNodes = []string{*argSeedNode}
+	// Create the client configurations.
+	configs := make([]*p2p.Config, *argClients)
+	for i := 0; i < *argClients; i++ {
+		configs[i] = p2p.DefaultConfig()
+		configs[i].DisableNATPortMap = *argDisableNATPortMap
+		configs[i].IP = *argIP
+		configs[i].KBucketSize = *argKBucketSize
+		configs[i].LogLevel = *argLevel
+		configs[i].Port = uint16(*argPort + uint(i))
+		if *argSeedNode != "" {
+			configs[i].SeedNodes = []string{*argSeedNode}
+		}
 	}
 
+	// Set the random seed.
+	if *argClients > 0 {
+		configs[0].RandomSeed = *argRandomSeed
+	}
+
+	// Lanuch clients.
+	group := &sync.WaitGroup{}
+	group.Add(*argClients)
+	for i := 0; i < *argClients; i++ {
+		go launch(configs[i], group, logger)
+	}
+
+	// Wait for clients to complete.
+	group.Wait()
+
+}
+
+func launch(config *p2p.Config, group *sync.WaitGroup, logger *logging.Logger) {
+
+	// Decrement the wait group counter on exit.
+	defer group.Done()
+
+	// Create a p2p client.
 	client, shutdown, err := config.New()
 	if err != nil {
-		logger.Critical("Cannot create network client:", err)
+		logger.Error("Cannot create p2p client:", err)
 		return
 	}
 	defer shutdown()
 
+	// Send and receive artifacts.
+	go send(client, logger)
+	go receive(client, logger)
+
+	// Hang.
+	// time.Sleep(5 * time.Minute)
+	select {}
+
+}
+
+func send(client p2p.Client, logger *logging.Logger) {
+
 	for {
-		if client.StreamCount() > 0 {
-			break
-		}
+
 		time.Sleep(time.Second)
+
+		n, err := rand.Int(rand.Reader, big.NewInt(math.MaxUint16))
+		if err != nil {
+			logger.Error("Cannot generate random integer:", err)
+			continue
+		}
+
+		data := make([]byte, n.Int64())
+		_, err = rand.Read(data)
+		if err != nil {
+			logger.Error("Cannot generate random bytes:", err)
+			continue
+		}
+
+		object, err := artifact.FromBytes(data, false)
+		if err != nil {
+			logger.Error("Cannot create artifact:", err)
+			continue
+		}
+
+		checksum := object.Checksum()
+		logger.Infof(
+			"Sending %d byte artifact with checksum %s",
+			object.Size(),
+			hex.EncodeToString(checksum[:4]),
+		)
+
+		client.Send(object)
+
 	}
 
-	go func() {
-		for {
-			time.Sleep(time.Second)
-			data := make([]byte, 32)
-			_, err := rand.Read(data)
-			if err != nil {
-				logger.Error("Cannot generate random data:", err)
-				continue
-			}
-			hash := sha256.Sum256(data)
-			logger.Debugf("Sending artifact <%s>", hex.EncodeToString(hash[:])[0:8])
-			object, err := artifact.FromBytes(data, false)
-			if err != nil {
-				logger.Error("Cannot create artifact:", err)
-				continue
-			}
-			client.Send(object)
-		}
-	}()
+}
 
-	go func() {
-		for {
-			object := client.Receive()
-			data, err := artifact.ToBytes(object)
-			if err != nil {
-				logger.Error("Cannot read artifact:", err)
-				continue
-			}
-			hash := sha256.Sum256(data)
-			logger.Debugf("Receiving artifact <%s>", hex.EncodeToString(hash[:])[0:8])
-		}
-	}()
+func receive(client p2p.Client, logger *logging.Logger) {
 
-	select {}
+	for {
+
+		object := client.Receive()
+
+		checksum := object.Checksum()
+		logger.Infof(
+			"Receiving %d byte artifact with checksum %s and latency %s",
+			object.Size(),
+			hex.EncodeToString(checksum[:4]),
+			time.Since(object.Timestamp()),
+		)
+
+		client.Send(object)
+
+	}
 
 }
