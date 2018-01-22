2587e6c44174bcc4cf9466af0d3fa546399a9361
Enzo Haussecker <enzo@sovereign.io>
2018-01-22 10:27:15 -0800

    Successful authentication to trigger routing table update

--- p2p/authenticate.go
+++ p2p/authenticate.go
@@ -15,12 +15,14 @@ import (
 	"gx/ipfs/QmXYjuNuxVzXKJCfWasQk1RqkhVLDM9jtUKhqc2WPQmFSB/go-libp2p-peer"
 )
 
-// Authenticate a peer.
+// Authenticate with a peer.
 func (client *client) auth(peerId peer.ID) (bool, error) {
 
+	// Log this action.
 	pid := peerId
-	client.logger.Debug("proving eligibility to", pid)
+	client.logger.Debug("Authenticating with", pid)
 
+	// Connect to the target peer.
 	stream, err := client.host.NewStream(
 		client.context,
 		pid,
@@ -28,50 +30,65 @@ func (client *client) auth(peerId peer.ID) (bool, error) {
 	)
 	if err != nil {
 		addrs := client.peerstore.PeerInfo(pid).Addrs
-		client.logger.Debug("cannot connect to", pid, "at", addrs, err)
+		client.logger.Debug("Cannot connect to", pid, "at", addrs, err)
 		client.peerstore.ClearAddrs(pid)
 		client.table.Remove(pid)
 		return false, err
 	}
 	defer stream.Close()
 
+	// Request a commitment and send it.
 	commitmentOut := client.requestCommitment()
 	err = client.sendCommitment(stream, commitmentOut)
 	if err != nil {
 		return false, err
 	}
 
+	// Receive a challenge.
 	challengeIn, err := client.receiveChallenge(stream)
 	if err != nil {
 		return false, err
 	}
 
+	// Request a zero-knowledge proof and send it.
 	proofOut := client.requestProof(commitmentOut, challengeIn)
 	err = client.sendProof(stream, proofOut)
 	if err != nil {
 		return false, err
 	}
 
-	client.logger.Debug("verifying eligibility of", pid)
-
+	// Receive a commitment.
 	commitmentIn, err := client.receiveCommitment(stream)
 	if err != nil {
 		return false, err
 	}
 
+	// Request a challenge and send it.
 	challengeOut := client.requestChallenge()
 	err = client.sendChallenge(stream, challengeOut)
 	if err != nil {
 		return false, err
 	}
 
+	// Receive a zero-knowledge proof.
 	proofIn, err := client.receiveProof(stream)
 	if err != nil {
 		return false, err
 	}
 
-	success := client.requestVerification(commitmentIn, challengeOut, proofIn)
+	// Verify the zero-knowledge proof.
+	success := client.requestVerification(
+		commitmentIn,
+		challengeOut,
+		proofIn,
+	)
+	if success {
+		client.peerstore.Put(pid, "AUTHORIZED", true)
+		client.peerstore.Put(pid, "AUTHORIZED_AT", time.Now())
+		client.table.Add(pid)
+	}
 
+	// Done.
 	return success, nil
 
 }
@@ -81,53 +98,63 @@ func (client *client) authHandler(stream net.Stream) {
 
 	defer stream.Close()
 
+	// Log this action.
 	pid := stream.Conn().RemotePeer()
-	client.logger.Debug("verifying eligibility of", pid)
+	client.logger.Debug("Authenticating with", pid)
 
+	// Check if the request is spam.
 	client.spammerCacheLock.Lock()
 	timestamp, exists := client.spammerCache.Get(pid)
 	if exists && time.Since(timestamp.(time.Time)) < 10*time.Minute {
 		client.spammerCacheLock.Unlock()
-		time.Sleep(client.config.Timeout)
+		time.Sleep(time.Second)
 		return
 	}
 	client.spammerCache.Add(pid, time.Now)
 	client.spammerCacheLock.Unlock()
 
+	// Receive a commitment.
 	commitmentIn, err := client.receiveCommitment(stream)
 	if err != nil {
 		return
 	}
 
+	// Request a challenge and send it.
 	challengeOut := client.requestChallenge()
 	err = client.sendChallenge(stream, challengeOut)
 	if err != nil {
 		return
 	}
 
+	// Receive a zero-knowledge proof.
 	proofIn, err := client.receiveProof(stream)
 	if err != nil {
 		return
 	}
 
+	// Verify the zero-knowledge proof.
 	success := client.requestVerification(commitmentIn, challengeOut, proofIn)
 	if !success {
 		return
 	}
+	client.peerstore.Put(pid, "AUTHORIZED", true)
+	client.peerstore.Put(pid, "AUTHORIZED_AT", time.Now())
+	client.table.Add(pid)
 
-	client.logger.Debug("Proving eligibility to", pid)
-
+	// Request a commitment and send it.
 	commitmentOut := client.requestCommitment()
 	err = client.sendCommitment(stream, commitmentOut)
 	if err != nil {
 		return
 	}
 
+	// Receive a challenge.
 	challengeIn, err := client.receiveChallenge(stream)
 	if err != nil {
 		return
 	}
 
+	// Request a zero-knowledge proof and send it.
 	proofOut := client.requestProof(commitmentOut, challengeIn)
 	err = client.sendProof(stream, proofOut)
 	if err != nil {
