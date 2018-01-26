215c08123c02b85d6659176a37c165ac602725d7
Enzo Haussecker <enzo@sovereign.io>
2018-01-25 16:53:46 -0800

    Separate report sending from event loop and add unit test

--- p2p/analytics.go
+++ p2p/analytics.go
@@ -30,57 +30,17 @@ func (client *client) activateAnalytics() func() {
 
 		for {
 
-			// Check for a shutdown instruction.
+			// Check for a shutdown notification.
 			select {
 			case <-notify:
 				return
 			default:
 			}
 
-			// Create a report.
-			report := analytics.Report{
-				ClusterID: client.config.ClusterID,
-				Network:   string(client.config.Network),
-				NodeID:    client.id.Pretty(),
-				Peers:     client.table.Size(),
-				ProcessID: client.config.ProcessID,
-				Timestamp: time.Now().Unix(),
-				UserData:  client.config.AnalyticsUserData,
-				Version:   string(client.config.Version),
-			}
-			for _, addr := range client.host.Addrs() {
-				report.Addrs = append(report.Addrs, addr.String())
-			}
-			for _, stream := range client.streamstore.Peers() {
-				report.Streams = append(report.Streams, stream.Pretty())
-			}
-
-			// Encode it.
-			data, err := json.Marshal(&report)
+			// Send an analytics report.
+			err := client.sendReport()
 			if err != nil {
-				client.logger.Warning("Cannot encode report", err)
-				return
-			}
-
-			// Create a request.
-			req, err := http.NewRequest(
-				"POST",
-				client.config.AnalyticsURL,
-				bytes.NewBuffer(data),
-			)
-			if err != nil {
-				client.logger.Warning("Cannot create request", err)
-				return
-			}
-			req.Header.Set("Content-Type", "application/json")
-
-			// Send it.
-			sender := &http.Client{}
-			resp, err := sender.Do(req)
-			if err != nil {
-				client.logger.Warning("Cannot send report", err)
-			} else {
-				resp.Body.Close()
+				client.logger.Warning("Cannot send analytics report", err)
 			}
 
 			// Wait.
@@ -92,5 +52,56 @@ func (client *client) activateAnalytics() func() {
 
 	// Return the shutdown function.
 	return shutdown
+
+}
+
+// Send an analytics report.
+func (client *client) sendReport() error {
+
+	// Create a report.
+	report := analytics.Report{
+		ClusterID: client.config.ClusterID,
+		Network:   string(client.config.Network),
+		NodeID:    client.id.Pretty(),
+		Peers:     client.table.Size(),
+		ProcessID: client.config.ProcessID,
+		Timestamp: time.Now().Unix(),
+		UserData:  client.config.AnalyticsUserData,
+		Version:   string(client.config.Version),
+	}
+	for _, addr := range client.host.Addrs() {
+		report.Addrs = append(report.Addrs, addr.String())
+	}
+	for _, stream := range client.streamstore.Peers() {
+		report.Streams = append(report.Streams, stream.Pretty())
+	}
+
+	// Encode it.
+	data, err := json.Marshal(&report)
+	if err != nil {
+		return err
+	}
+
+	// Create a request.
+	req, err := http.NewRequest(
+		"POST",
+		client.config.AnalyticsURL,
+		bytes.NewBuffer(data),
+	)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send it.
+	sender := &http.Client{}
+	resp, err := sender.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	// Done.
+	return nil
 
 }
---
+++ p2p/analytics_test.go
@@ -0,0 +1,120 @@
+/**
+ * File        : analytics_test.go
+ * Description : Unit test.
+ * Copyright   : Copyright (c) 2017-2018 DFINITY Stiftung. All rights reserved.
+ * Maintainer  : Enzo Haussecker <enzo@dfinity.org>
+ * Stability   : Experimental
+ */
+
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"testing"
+
+	"gx/ipfs/QmPgDWmTmuzvP7QE5zwo1TmjbJme9pmZHNujB2453jkCTr/go-libp2p-peerstore"
+
+	"github.com/dfinity/go-revolver/analytics"
+)
+
+// Show that clients can report their streams to an analytics server.
+func TestAnalytics(test *testing.T) {
+
+	// Create a simple key-value store for analytics reports.
+	reports := make(map[string]analytics.Report)
+	lock := &sync.Mutex{}
+
+	// Create a TCP listener to be used by the analytics server.
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		test.Fatal(err)
+	}
+	defer listener.Close()
+
+	// Start the analytics server on a separate thread.
+	go func(reports map[string]analytics.Report, lock *sync.Mutex, listener net.Listener) {
+		http.HandleFunc("/report", analytics.ReportHandler(reports, lock))
+		http.Serve(listener, nil)
+	}(reports, lock, listener)
+
+	// Define the URL used for report submission.
+	url := fmt.Sprintf(
+		"http://127.0.0.1:%d/report",
+		listener.Addr().(*net.TCPAddr).Port,
+	)
+
+	// Create a client.
+	client1 := newAnalyticsClient(test, url)
+	defer client1.Close()
+
+	// Create a second client.
+	client2 := newAnalyticsClient(test, url)
+	defer client2.Close()
+
+	// Add the second client to the peer store of the first.
+	client1.peerstore.AddAddrs(
+		client2.id,
+		client2.host.Addrs(),
+		peerstore.ProviderAddrTTL,
+	)
+
+	// Add the first client to the routing table of the second.
+	client2.table.Add(client1.id)
+
+	// Pair the clients. This will allow them to exchange artifacts.
+	success, err := client1.pair(client2.id)
+	if err != nil {
+		test.Fatal(err)
+	}
+	if !success {
+		test.Fatal("Cannot negotiate artifact exchange")
+	}
+
+	// Send an analytics report.
+	err = client1.sendReport()
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	// Locate the analytics report.
+	lock.Lock()
+	report, exists := reports[client1.id.Pretty()]
+	lock.Unlock()
+	if !exists {
+		test.Fatal("Cannot locate analytics report")
+	}
+
+	// Verify that the report references the artifact stream.
+	if len(report.Streams) == 0 {
+		test.Fatal("Missing reference to artifact stream")
+	}
+	if report.Streams[0] != client2.id.Pretty() {
+		test.Fatal("Unexpected reference to artifact stream")
+	}
+
+}
+
+func newAnalyticsClient(test *testing.T, url string) *client {
+
+	// Configure an analytics client.
+	config := DefaultConfig()
+	config.AnalyticsURL = url
+	config.DisableAnalytics = true
+	config.DisableNATPortMap = true
+	config.DisablePeerDiscovery = true
+	config.DisableStreamDiscovery = true
+	config.IP = "127.0.0.1"
+
+	// Instantiate the client.
+	client, err := config.create()
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	// Ready for action!
+	return client
+
+}
